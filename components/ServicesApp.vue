5cc3f57d084e4ba066ed260ea932bc7b6843df93
1Alusha1 <alusha9636@gmail.com>
2023-02-14 16:44:39 +0200

    created block services

---
+++ components/ServicesApp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="services">
+    <h2 class="services__title"></h2>
+    <h3 class="services__subtitle"></h3>
+    <div class="cards-wrap">
+      <card-app v-for="item in 8" :key="item">
+        <img :src="require('../assets/images/Rectangle.png')" alt="" />
+        <div class="card-info">
+          <div class="card-title">Маникюр + Однотонное покрытиие гель-лак</div>
+          <div class="card-price">400 грн</div>
+          <div class="card-description">
+            Аппаратный/комбинированный маникюр и однотонное покрытие гель-лак
+          </div>
+        </div>
+      </card-app>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardApp from './ui/CardApp.vue'
+export default {
+  components: { CardApp },
+  data() {
+    return {
+      arr: [1, 2, 3, 4, 5, 6],
+    }
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container">
     <header-app />
+    <services-app />
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import Header from '~/components/HeaderApp.vue'
+import ServicesApp from '~/components/ServicesApp.vue'
 
 export default Vue.extend({
+  components: { ServicesApp },
   name: 'IndexPage',
 })
 Header
